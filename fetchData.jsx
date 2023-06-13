921d56d6e3aad5d5d011d42929786e3ffcf36c0f
jamesmacalino <hootenmeyer@gmail.com>
2023-06-13 03:25:56 -0400

    --

--- fetchData.jsx
+++ fetchData.jsx
@@ -87,11 +87,11 @@ const dataFetchReducer = (state, action) => {
 // App that gets data from Hacker News url
 function App() {
   const { Fragment, useState, useEffect, useReducer } = React;
-  const [query, setQuery] = useState("MIT");
+  const [query, setQuery] = useState("NOAA");
   const [currentPage, setCurrentPage] = useState(1);
   const pageSize = 10;
   const [{ data, isLoading, isError }, doFetch] = useDataApi(
-    "https://hn.algolia.com/api/v1/search?query=MIT",
+    "https://hn.algolia.com/api/v1/search?query=noaa",
     {
       hits: [],
     }
